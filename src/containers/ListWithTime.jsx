07145a101bba9d765e095a6f50178e43b8a4be33
jeromebienaime <jerome.bienaime@bulle-communication.com>
2020-02-11 14:03:28 +0000

    Adds proper duration in history

--- src/containers/ListWithTime.jsx
+++ src/containers/ListWithTime.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
+import Interval from 'luxon/src/interval.js'
 import React from "react"
 
 const List = ({ list }) => (
     <ul>
-        {list.length && list.map(item => <li key={item.id}><p>{item.label} - done in {/* @TODO duration: stopped_at - started_at, might need momentjs or https://moment.github.io/luxon/*/} seconds ({item.started_at.toString()}, {item.stopped_at.toString()})</p><p>total time in seconds: {item.time}</p></li>)}
+        {list.length && list.map(item => <li key={item.id}><p>{item.label} - done in {Interval.fromDateTimes(item.started_at, item.stopped_at).toDuration('seconds').toObject().seconds} seconds ({item.started_at.toString()}, {item.stopped_at.toString()})</p><p>total time in seconds: {item.time}</p></li>)}
     </ul>
 )
 
