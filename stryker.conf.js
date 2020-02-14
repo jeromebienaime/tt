e6cbe9a2f934e20fc07433bf57d058cd511e546c
jeromebienaime <jerome.bienaime@bulle-communication.com>
2020-02-14 14:38:05 +0000

    Updates concurrent runner

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,10 +2,10 @@
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
 module.exports = function (config) {
     config.set({
-        mutate: ["src/**/*.js?(x)", "!src/**/*@(.test|.spec|Spec).js?(x)"],
+        mutate: ["src/**/*.js?(x)", "!src/**/*@(.test|.spec|Spec).js?(x)", "!src/validators/**/*.js"],
         mutator: "javascript",
         // testRunner: "jest",
-        reporters: ["progress", "clear-text", "html"],
+        reporters: ["progress", "clear-text", "html", "dashboard"],
         // coverageAnalysis: "perTest",
         /* jest: {
             projectType: "react"
@@ -16,6 +16,7 @@ module.exports = function (config) {
         dashboard: {
             reportType: "full"
         },
-        loglevel: "all"
+        loglevel: "all",
+        maxConcurrentTestRunners: 2
     })
 }
